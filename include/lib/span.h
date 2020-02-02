588325bfc76804c31b0df5fa09e61641c92efc93
rgreenblatt <greenblattryan@gmail.com>
2020-02-02 12:22:22 -0500

    Better approach to debug span

--- include/lib/span.h
+++ include/lib/span.h
@@ -5,44 +5,40 @@
 #include <assert.h>
 #include <type_traits>
 
-#ifdef NDEBUG
-#define DEFAULT_IGNORE true
-#else
-#define DEFAULT_IGNORE false
-#endif
-
-template <typename T, bool ignore_size = DEFAULT_IGNORE> class Span {
-#undef DEFAULT_IGNORE
+template <typename T, bool is_sized = false> class Span {
 public:
   constexpr Span(T *ptr, std::size_t size) : ptr_(ptr) {
-    if constexpr (!ignore_size) {
+    if constexpr (use_size) {
       size_ = size;
     }
   }
 
   template <typename V>
   constexpr Span(V &v) : ptr_(SpanConvertable<V>::ptr(v)) {
-    if constexpr (!ignore_size) {
+    if constexpr (use_size) {
       size_ = SpanConvertable<V>::size(v);
     }
   }
 
   template <typename V>
   constexpr Span(const V &v) : ptr_(SpanConvertable<V>::ptr(v)) {
-    if constexpr (!ignore_size) {
+    if constexpr (use_size) {
       size_ = SpanConvertable<V>::size(v);
     }
   }
 
   constexpr Span() {}
 
-  constexpr std::size_t size() const { return size_; }
+  constexpr std::size_t size() const {
+    static_assert(is_sized, "size method can't be used if span isn't sized");
+    return size_;
+  }
 
   constexpr bool checkIndex(const std::size_t index) const {
-    if constexpr (ignore_size) {
-      return true;
-    } else {
+    if constexpr (is_debug) {
       return index < size_;
+    } else {
+      return true;
     }
   }
 
@@ -55,24 +51,34 @@ public:
 
   constexpr T *begin() const { return ptr_; }
 
-  constexpr T *end() const { return ptr_ + size_; }
+  constexpr T *end() const { return ptr_ + size(); }
 
 private:
   T *ptr_;
 
   struct NoSize {};
 
-  typename std::conditional<ignore_size, NoSize, std::size_t>::type size_;
+  static constexpr bool is_debug =
+#ifdef NDEBUG
+      false
+#else
+      true
+#endif
+      ;
+  static constexpr bool use_size = is_sized || is_debug;
+
+  typename std::conditional_t<use_size, std::size_t, NoSize> size_;
+
+  friend class SpanConvertable<Span>;
 };
 
-template <typename T, bool ignore_size>
-class SpanConvertable<Span<T, ignore_size>> {
+template <typename T, bool is_sized> class SpanConvertable<Span<T, is_sized>> {
 public:
-  constexpr static T *ptr(const Span<T, ignore_size> &v) { return v.data(); }
+  constexpr static T *ptr(const Span<T, is_sized> &v) { return v.ptr_; }
 
-  constexpr static std::size_t size(const Span<T, ignore_size> &v) {
-    return v.size();
+  constexpr static std::size_t size(const Span<T, is_sized> &v) {
+    return v.size_;
   }
 };
 
-template <typename T> using SpanSized = Span<T, false>;
+template <typename T> using SpanSized = Span<T, true>;
