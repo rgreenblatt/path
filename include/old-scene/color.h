9a10a6165e036c5b6edcbf5ffd426670159e278c
rgreenblatt <greenblattryan@gmail.com>
2020-02-19 00:26:00 -0500

    Remove old scene

--- include/old-scene/color.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Eigen/Core>
-
-namespace scene {
-using Color = Eigen::Array3f;
-}
--- include/old-scene/shape.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stddef.h>
-
-namespace scene {
-enum class Shape { Sphere, Cylinder, Cube, Cone };
-static constexpr size_t shapes_size = 1;
-} // namespace scene
