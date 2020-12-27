c9c0b4d91c128e07ee1463964d5df927f9ad864a
rgreenblatt <greenblattryan@gmail.com>
2020-12-27 15:27:23 -0500

    Add comments

--- include/lib/assert.h
+++ include/lib/assert.h
@@ -1,16 +1,21 @@
 #pragma once
 
-#include <cassert> 
+#include <cassert>
 
+// assert which is disabled by NDEBUG
 #define debug_assert(expr) assert(expr)
 
+// assert which is never disabled
 #define always_assert(expr)                                                    \
   (static_cast<bool>(expr)                                                     \
        ? void(0)                                                               \
        : __assert_fail(#expr, __FILE__, __LINE__, __ASSERT_FUNCTION))
 
+// assert that a section is unreachable or if NDEBUG is defined
+// don't assert, cause UB if section is reached
 #define unreachable_unchecked()                                                \
   debug_assert(false);                                                         \
   __builtin_unreachable();
 
+// assert that section is unreachable (always)
 #define unreachable() always_assert(false);
