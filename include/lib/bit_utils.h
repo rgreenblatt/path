8cfe5c7cd1626b69732358911c55008e76296af9
rgreenblatt <greenblattryan@gmail.com>
2020-12-29 12:48:40 -0500

    Some changes to bit utils

--- include/lib/bit_utils.h
+++ include/lib/bit_utils.h
@@ -1,60 +1,111 @@
 #pragma once
 
-#include "lib/cuda/utils.h"
+#include "lib/assert.h"
 
 #include <concepts>
 #include <limits>
-
-inline HOST_DEVICE uint32_t popcount(uint32_t v) {
-  static_assert(sizeof(uint32_t) == sizeof(unsigned));
-  static_assert(sizeof(uint32_t) == sizeof(int));
-#ifdef __CUDA_ARCH__
-  // this type is technically int (signed), but this works on all uint32_t (see
-  // tests)
-  return __popc(v);
-#else
-  return __builtin_popcount(v);
-#endif
-}
-
-inline HOST_DEVICE uint64_t popcount(uint64_t v) {
-  static_assert(sizeof(uint64_t) == sizeof(unsigned long long));
-  static_assert(sizeof(uint64_t) == sizeof(long long int));
-#ifdef __CUDA_ARCH__
-  // this type is technically long long int (signed), but this works on all
-  // uint64_t (see tests)
-  return __popcll(v);
-#else
-  return __builtin_popcountll(v);
-#endif
-}
-
-inline HOST_DEVICE unsigned count_leading_zeros(uint32_t v) {
-  static_assert(sizeof(uint32_t) == sizeof(unsigned));
-#ifdef __CUDA_ARCH__
-  return __clz(v);
-#else
-  return __builtin_clz(v);
-#endif
-}
-
-inline HOST_DEVICE unsigned count_leading_zeros(uint64_t v) {
-  static_assert(sizeof(uint64_t) == sizeof(unsigned long long));
-#ifdef __CUDA_ARCH__
-  return __clzll(v);
-#else
-  return __builtin_clzll(v);
-#endif
-}
+#include <climits>
+#include <cstdint>
 
 template <std::integral T>
-static constexpr unsigned bits_per = sizeof(T) * CHAR_BIT;
+inline constexpr unsigned bits_per = sizeof(T) * CHAR_BIT;
 
-template <std::integral T> constexpr T bit_mask(unsigned bit_idx) {
+template<std::unsigned_integral T>
+requires (bits_per<T> <= bits_per<unsigned long long>)
+inline constexpr uint8_t popcount(T v) {
+  if constexpr (bits_per<T> <= bits_per<unsigned>) {
+    return __builtin_popcount(v);
+  } else if constexpr (bits_per<T> <= bits_per<unsigned long>) {
+    return __builtin_popcountl(v);
+  } else {
+    return __builtin_popcountll(v);
+  }
+}
+
+// use static_assert for examples and testing
+static_assert(popcount(0b1100u) == 2);
+static_assert(popcount(0b0u) == 0);
+static_assert(popcount(0b11111110u) == 7);
+
+// note that the exact type matters a lot for the behavior of this function
+// see examples below
+template<std::unsigned_integral T>
+requires (bits_per<T> <= bits_per<unsigned long long>)
+inline constexpr uint8_t count_leading_zeros(T v) {
+  debug_assert_assume(v != 0);
+  if constexpr (bits_per<T> <= bits_per<unsigned>) {
+    return __builtin_clz(v) - (bits_per<unsigned> - bits_per<T>);
+  } else if constexpr (bits_per<T> <= bits_per<unsigned long>) {
+    return __builtin_clzl(v) - (bits_per<unsigned long> - bits_per<T>);
+  } else {
+    return __builtin_clzll(v) - (bits_per<unsigned long long> - bits_per<T>);
+  }
+}
+
+static_assert(count_leading_zeros(static_cast<uint8_t>(0b1u)) == 7);
+static_assert(count_leading_zeros(static_cast<uint16_t>(0b1u)) == 15);
+static_assert(count_leading_zeros(static_cast<uint16_t>(0b1111u)) == 12);
+static_assert(count_leading_zeros(0b1u) == 31);
+static_assert(count_leading_zeros(static_cast<uint64_t>(0b1u)) == 63);
+static_assert(count_leading_zeros(0b100u) == 29);
+static_assert(count_leading_zeros(0b10111u) == 27);
+
+template<std::unsigned_integral T>
+constexpr bool power_of_2(T n) {
+  return (n > 0 && ((n & (n - 1)) == 0));
+};
+
+static_assert(!power_of_2(0b0u));
+static_assert(power_of_2(0b1u));
+static_assert(power_of_2(0b10u));
+static_assert(power_of_2(0b100000u));
+static_assert(!power_of_2(0b100100u));
+static_assert(!power_of_2(0b111111u));
+static_assert(!power_of_2(0b111u));
+static_assert(!power_of_2(0b101u));
+
+template<std::unsigned_integral T>
+inline constexpr T closest_power_of_2(T n) {
+  if (n == 0) {
+    return 1;
+  }
+  unsigned n_leading_zeros = count_leading_zeros(n);
+  debug_assert_assume(n_leading_zeros >= 0);
+  debug_assert_assume(n_leading_zeros <= bits_per<T> - 1);
+  unsigned place_of_leading_bit = bits_per<T> - 1 - n_leading_zeros;
+  T smaller_power_of_2 = T(1) << place_of_leading_bit;
+  debug_assert_assume(smaller_power_of_2 <= n);
+  debug_assert_assume(power_of_2(smaller_power_of_2));
+  if (place_of_leading_bit == bits_per<T> - 1) {
+    return smaller_power_of_2;
+  }
+  unsigned larger_power_of_2 = smaller_power_of_2 << 1;
+  debug_assert_assume(larger_power_of_2 > n);
+  debug_assert_assume(power_of_2(larger_power_of_2));
+  if (n - smaller_power_of_2 <= larger_power_of_2 - n) {
+    return smaller_power_of_2;
+  } else {
+    return larger_power_of_2;
+  }
+}
+
+static_assert(closest_power_of_2(0b100u) == 0b100u);
+static_assert(closest_power_of_2(0b0u) == 0b1u);
+static_assert(closest_power_of_2(0b101u) == 0b100u);
+static_assert(closest_power_of_2(0b111u) == 0b1000u);
+static_assert(closest_power_of_2(std::numeric_limits<unsigned>::max()) ==
+              1u << 31);
+
+template <std::unsigned_integral T> constexpr T bit_mask(unsigned bit_idx) {
   return T(1) << bit_idx;
 }
 
-template <std::integral T> constexpr T up_to_mask(unsigned n) {
+static_assert(bit_mask<unsigned>(0) == 0b1);
+static_assert(bit_mask<unsigned>(2) == 0b100);
+static_assert(bit_mask<unsigned>(7) == 0b10000000);
+static_assert(bit_mask<unsigned>(31) == 0b10000000000000000000000000000000u);
+
+template <std::unsigned_integral T> constexpr T up_to_mask(unsigned n) {
   // in binary, 1 (n + 1) times
   // 0: 1
   // 1: 11
@@ -64,3 +115,8 @@ template <std::integral T> constexpr T up_to_mask(unsigned n) {
   return n >= bits_per<T> - 1 ? std::numeric_limits<T>::max()
                               : bit_mask<T>(n + T(1)) - T(1);
 }
+
+static_assert(up_to_mask<unsigned>(0) == 0b1);
+static_assert(up_to_mask<unsigned>(1) == 0b11);
+static_assert(up_to_mask<unsigned>(7) == 0b11111111);
+static_assert(up_to_mask<unsigned>(31) == 0b11111111111111111111111111111111u);
---
+++ tests/src/bit_utils.cu
@@ -0,0 +1,85 @@
+#include "lib/bit_utils.h"
+#include "execution_model/host_device_vector.h"
+#include "lib/span.h"
+
+#include <gtest/gtest.h>
+
+#include <random>
+
+// These tests mostly exist to verify that the gpu functions
+// work as expected. Basic unit testing is included in bit_utils.h
+// in the form of static_assert
+template <typename T> static void popcount_test(std::mt19937 &gen) {
+  const unsigned size = 1000;
+  HostDeviceVector<T> values(size);
+  std::uniform_int_distribution<T> dis(0, std::numeric_limits<T>::max());
+  std::generate(values.begin(), values.end(), [&] { return dis(gen); });
+
+  values[0] = 0b10100111u;
+  values[1] = std::numeric_limits<T>::max();
+
+  HostDeviceVector<T> gpu_out(size);
+  HostDeviceVector<T> cpu_out(size);
+
+  auto transform_values = [&](const auto type, HostDeviceVector<T> &out) {
+    thrust::transform(type, values.data(), values.data() + values.size(),
+                      out.data(),
+                      [] __host__ __device__(T v) { return popcount(v); });
+  };
+
+  transform_values(thrust::host, cpu_out);
+  transform_values(thrust::device, gpu_out);
+
+  for (unsigned i = 0; i < size; i++) {
+    EXPECT_EQ(gpu_out[i], cpu_out[i]);
+  }
+
+  EXPECT_EQ(gpu_out[0], T(5));
+  EXPECT_EQ(cpu_out[0], T(5));
+  T max_value_bits = sizeof(T) * CHAR_BIT;
+  EXPECT_EQ(gpu_out[1], max_value_bits);
+  EXPECT_EQ(cpu_out[1], max_value_bits);
+}
+
+TEST(Bitset, popcount) {
+  std::mt19937 gen(testing::UnitTest::GetInstance()->random_seed());
+
+  popcount_test<uint8_t>(gen);
+  popcount_test<uint16_t>(gen);
+  popcount_test<uint32_t>(gen);
+  popcount_test<uint64_t>(gen);
+}
+
+template <typename T> static void count_leading_zeros_test(std::mt19937 &gen) {
+  const unsigned size = 1000;
+  HostDeviceVector<T> values(size);
+  std::uniform_int_distribution<T> dis(0u, std::numeric_limits<T>::max());
+  std::uniform_int_distribution<T> dis_mask(0u, 31);
+  std::generate(values.begin(), values.end(),
+                [&] { return (dis(gen) & ((1u << dis_mask(gen)) - 1)) | 1u; });
+
+  HostDeviceVector<T> gpu_out(size);
+  HostDeviceVector<T> cpu_out(size);
+
+  auto transform_values = [&](const auto type, HostDeviceVector<T> &out) {
+    thrust::transform(
+        type, values.data(), values.data() + values.size(), out.data(),
+        [] __host__ __device__(T v) { return count_leading_zeros(v); });
+  };
+
+  transform_values(thrust::host, cpu_out);
+  transform_values(thrust::device, gpu_out);
+
+  for (unsigned i = 0; i < size; i++) {
+    EXPECT_EQ(gpu_out[i], cpu_out[i]);
+  }
+}
+
+TEST(Bitset, count_leading_zeros) {
+  std::mt19937 gen(testing::UnitTest::GetInstance()->random_seed());
+
+  count_leading_zeros_test<uint8_t>(gen);
+  count_leading_zeros_test<uint16_t>(gen);
+  count_leading_zeros_test<uint32_t>(gen);
+  count_leading_zeros_test<uint64_t>(gen);
+}
