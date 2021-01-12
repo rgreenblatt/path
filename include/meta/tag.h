d5d03196dc98324676767a235361d1fda6d384ad
rgreenblatt <greenblattryan@gmail.com>
2021-01-12 12:51:34 -0500

    Added tag type

--- include/meta/tag.h
+++ include/meta/tag.h
@@ -4,7 +4,7 @@
 #include "meta/get_idx.h"
 
 // allow for using an index directly
-template<unsigned idx_in> struct NTag {
+template <unsigned idx_in> struct NTag {
   static constexpr unsigned idx = idx_in;
 };
 
@@ -19,6 +19,18 @@ requires(idx_in < AllValues<E>.size()) struct Tag {
   Tag(NTag<idx_in>) {}
 };
 
-// convenience macro for using tags
-#define TAG(tag)                                                               \
-  Tag<std::decay_t<decltype(tag)>, get_idx(tag)> {}
+template <auto value_in>
+requires(AllValuesEnumerable<decltype(value_in)> &&get_idx(value_in) <
+         AllValues<decltype(value_in)>.size()) struct TTag {
+  static constexpr unsigned idx = get_idx(value_in);
+  static constexpr auto value = value_in;
+
+  // implicit conversion - must be defined here because TTag may not
+  // exist (if type isn't structural) while Tag always exists...
+  operator Tag<decltype(value_in), idx>() const { return {}; }
+  TTag (Tag<decltype(value_in), idx>) { }
+};
+
+// convenience macros for using tags
+#define TAGT(tag) Tag<std::decay_t<decltype(tag)>, get_idx(tag)>
+#define TAG(tag) TAGT(tag)()
