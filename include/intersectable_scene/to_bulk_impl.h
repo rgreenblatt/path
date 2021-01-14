bc734bbd32333226d64aa1ea37f33101032bf0ac
rgreenblatt <greenblattryan@gmail.com>
2021-01-14 00:04:12 -0500

    Fix bug

--- include/intersectable_scene/to_bulk_impl.h
+++ include/intersectable_scene/to_bulk_impl.h
@@ -8,10 +8,12 @@ namespace intersectable_scene {
 template <ExecutionModel exec, intersect::Intersectable I>
 Span<const typename ToBulkGen<exec, I>::IntersectionOp>
 ToBulkGen<exec, I>::get_intersections() {
+  // TODO: consider if it would be better to use a custom thread division
+  // instead of thrust::transform...
   thrust::transform(
       rays_.begin(), rays_.end(), intersections_.begin(),
       [intersectable = scene_intersectable_->intersectable](
-          const intersect::Ray &ray) { intersectable.intersect(ray); });
+          const intersect::Ray &ray) { return intersectable.intersect(ray); });
 
   return intersections_;
 }
