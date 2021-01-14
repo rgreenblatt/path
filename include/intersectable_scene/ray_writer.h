c41bfd15d6d47d1c6e2bac0c96e356e3472174af
rgreenblatt <greenblattryan@gmail.com>
2021-01-14 00:05:29 -0500

    make ray writer const

--- include/intersectable_scene/ray_writer.h
+++ include/intersectable_scene/ray_writer.h
@@ -7,13 +7,16 @@
 
 namespace intersectable_scene {
 template <typename T>
-concept RayWriter = requires(T &t, unsigned idx, const intersect::Ray &ray) {
+concept RayWriter = requires(const T &t, unsigned idx,
+                             const intersect::Ray &ray) {
   t.write_at(idx, ray);
 };
 
 struct SpanRayWriter {
   Span<intersect::Ray> rays;
 
-  void write_at(unsigned idx, const intersect::Ray &ray) { rays[idx] = ray; }
+  void write_at(unsigned idx, const intersect::Ray &ray) const {
+    rays[idx] = ray;
+  }
 };
 } // namespace intersectable_scene
