478a81399331209255f43636c9ba05e893845b05
rgreenblatt <greenblattryan@gmail.com>
2021-01-14 11:46:06 -0500

    Cleanup to allow constructing types which aren't default initializable

--- include/meta/per_instance.h
+++ include/meta/per_instance.h
@@ -9,13 +9,16 @@ template <AllValuesEnumerable T, template <T> class TypeOver,
           template <typename...> class V>
 struct PerInstanceImpl {
   // this approach is a bit gross, but I can't think of a better one...
-  template <std::size_t... i>
-  static constexpr auto items_helper(std::integer_sequence<std::size_t, i...>) {
-    return V<TypeOver<AllValues<T>[i]>...>{};
-  }
+  template <std::size_t... i> struct ItemsHelper {
+    using Type = V<TypeOver<AllValues<T>[i]>...>;
+    constexpr ItemsHelper(std::integer_sequence<std::size_t, i...>) {}
+  };
 
-  using type = decltype(PerInstanceImpl::items_helper(
-      std::make_index_sequence<AllValues<T>.size()>{}));
+  template <std::size_t... i>
+  ItemsHelper(std::integer_sequence<std::size_t, i...>) -> ItemsHelper<i...>;
+
+  using type = typename decltype(ItemsHelper(
+      std::make_index_sequence<AllValues<T>.size()>{}))::Type;
 };
 } // namespace detail
 } // namespace per_instance
