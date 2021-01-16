45a9af9a8faab2b78f63b9696c63b60e2a8cd7cc
rgreenblatt <greenblattryan@gmail.com>
2021-01-15 20:25:12 -0500

    Don't disable asserts for constexpr

--- include/lib/assert.h
+++ include/lib/assert.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <type_traits>
 
 extern "C" {
 // always forward declare __assert_fail regardless of NDEBUG
@@ -9,22 +10,33 @@ extern void __assert_fail(const char *__assertion, const char *__file,
     __attribute__((__noreturn__));
 }
 
-// assert which is disabled by NDEBUG
-#define debug_assert(expr) assert(expr)
-
-// debug assert which allows the compile to assume the statement is always true
-// when NDEBUG is enabled
-#ifdef NDEBUG
-#define debug_assert_assume(expr) __builtin_assume(expr);
-#else
-#define debug_assert_assume(expr) debug_assert(expr)
-#endif
-
 // assert which is never disabled
 #define always_assert(expr)                                                    \
   (static_cast<bool>(expr) ? void(0)                                           \
                            : __assert_fail(#expr, __FILE__, __LINE__,          \
                                            __extension__ __PRETTY_FUNCTION__))
+
+// assert which is disabled by NDEBUG, unless in constant-evaluated context
+#define debug_assert(expr)                                                     \
+  (std::is_constant_evaluated() ? always_assert(expr) : assert(expr))
+
+#if defined(__clang__)
+#define ASSUME(expr) __builtin_assume(expr)
+#elif defined(_MSC_VER)
+#define ASSUME(expr) __assume(expr)
+#else
+#define ASSUME(expr) void(0)
+#endif
+
+// debug assert which allows the compile to assume the statement is always true
+// when NDEBUG is enabled
+#ifdef NDEBUG
+// include debug_assert in this case to catch issues in constant-evaluated
+// contexts
+#define debug_assert_assume(expr) (debug_assert(expr), ASSUME(expr))
+#else
+#define debug_assert_assume(expr) debug_assert(expr)
+#endif
 
 // assert that a section is unreachable or if NDEBUG is defined
 // don't assert, cause UB if section is reached
