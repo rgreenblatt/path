b688651e299bbc6d8e00f1c80a380dbd532fcbe3
rgreenblatt <greenblattryan@gmail.com>
2021-01-16 13:18:09 -0500

    Mild tagged tuple clean up and add AllValuesImpl

--- include/lib/tagged_tuple.h
+++ include/lib/tagged_tuple.h
@@ -6,30 +6,34 @@
 #include "meta/per_instance.h"
 #include "meta/sequential_dispatch.h"
 #include "meta/tag.h"
+#include "meta/tuple.h"
+#include "meta/all_values_tuple.h"
+
+#include <boost/hana/unpack.hpp>
+#include <boost/hana/ext/std/array.hpp>
 
 #include <compare>
-#include <tuple>
-#include <utility>
+#include <array>
 
 template <AllValuesEnumerable E, typename... T> struct TaggedTuple {
-  std::tuple<T...> items;
+  MetaTuple<T...> items;
 
   template <unsigned idx>
   ATTR_PURE_NDEBUG constexpr decltype(auto) get(Tag<E, idx>) {
-    return std::get<idx>(items);
+    return items[boost::hana::size_c<idx>];
   }
 
   template <unsigned idx>
   ATTR_PURE_NDEBUG constexpr decltype(auto) get(Tag<E, idx>) const {
-    return std::get<idx>(items);
+    return items[boost::hana::size_c<idx>];
   }
 
-  template <typename F> constexpr auto visit(const F &f, const E &value) {
-    return visit_impl(f, value, *this);
+  template <typename F> constexpr auto visit(F &&f, const E &value) {
+    return visit_impl(std::forward<F>(f), value, *this);
   }
 
-  template <typename F> constexpr auto visit(const F &f, const E &value) const {
-    return visit_impl(f, value, *this);
+  template <typename F> constexpr auto visit(F &&f, const E &value) const {
+    return visit_impl(std::forward<F>(f), value, *this);
   }
 
   constexpr bool operator==(const TaggedTuple &other) const = default;
@@ -37,12 +41,19 @@ template <AllValuesEnumerable E, typename... T> struct TaggedTuple {
 
 private:
   template <typename F, DecaysTo<TaggedTuple> V>
-  static constexpr auto visit_impl(const F &f, const E &type, V &&v) {
+  static constexpr auto visit_impl(F &&f, const E &type, V &&v) {
     return sequential_dispatch<AllValues<E>.size()>(
-        get_idx(type),
-        [&]<unsigned idx>(NTag<idx>) { return f(std::get<idx>(v.items)); });
+        get_idx(type), [&](auto tag) { return f(v.get(to_tag<E>(tag))); });
   }
 };
 
 template <AllValuesEnumerable T, template <T> class TypeOver>
 using TaggedTuplePerInstance = PerInstanceTakesType<T, TypeOver, TaggedTuple>;
+
+template<AllValuesEnumerable E, typename... T>
+struct AllValuesImpl<TaggedTuple<E, T...>> {
+  static constexpr auto values =
+      boost::hana::unpack(AllValues<MetaTuple<T...>>, [](auto... values) {
+        return std::array{TaggedTuple<E, T...>{values}...};
+      });
+};
