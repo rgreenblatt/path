398415e5e1400572d0e4bc53147b9e3595f8fd56
rgreenblatt <greenblattryan@gmail.com>
2021-01-20 17:57:11 -0500

    Minor improvements to static sized str

--- include/meta/static_sized_string.h
+++ include/meta/static_sized_string.h
@@ -7,77 +7,154 @@
 #include <compare>
 #include <type_traits>
 
-constexpr size_t constexpr_strlen(const char *v) {
-  const char *s;
+namespace static_sized_str {
+template <typename C> constexpr size_t constexpr_strlen(const C *v) {
+  const C *s = v;
 
-  for (s = v; *s; ++s) {
+  for (; *s; ++s) {
   }
 
   return (s - v);
 }
 
-template <unsigned size_in> class StaticSizedStr {
+template <unsigned n> using IdxT = std::integral_constant<unsigned, n>;
+
+template <unsigned size_in, typename C = char> class StaticSizedStr {
 public:
-  template <unsigned other_size>
-  constexpr auto operator+(const StaticSizedStr<other_size> &other) const {
-    debug_assert(data_[size_in] == '\0');
-    debug_assert(other.data_[other_size] == '\0');
+  // zero initialize to ensure null termination
+  constexpr StaticSizedStr() : data_{} {}
 
-    StaticSizedStr<size_in + other_size> out;
-    std::copy(data_.begin(), data_.end() - 1, out.data_.begin());
-    std::copy(other.data_.begin(), other.data_.end(),
-              out.data_.begin() + size_in);
-    debug_assert(out.data_[size_in + other_size] == '\0');
-
-    return out;
-  }
-
-  static constexpr unsigned size() { return size_in; }
-
-  constexpr StaticSizedStr(const char *v) {
+  constexpr StaticSizedStr(const C *v) {
     std::copy(v, v + size_in, data_.begin());
     data_[size_in] = '\0';
   }
 
-  constexpr StaticSizedStr(std::string_view v) {
-    std::copy(v.begin(), v.begin() + size_in, data_.begin());
-    data_[size_in] = '\0';
-  }
-
-  template <const char *v>
+  template <const C *v>
   requires(constexpr_strlen(v) == size_in) constexpr StaticSizedStr(
-      std::integral_constant<const char *, v>) {
-    return StaticSizedStr(v);
+      std::integral_constant<const C *, v>)
+      : StaticSizedStr(v) {}
+
+  constexpr StaticSizedStr(std::string_view v) : StaticSizedStr(v.data()) {}
+
+  static constexpr unsigned size() { return size_in; }
+
+  constexpr const C *c_str() const { return data_.data(); }
+
+  constexpr std::string_view as_view() const { return {c_str(), size_in}; }
+
+  template <unsigned other_size>
+  constexpr auto operator+(const StaticSizedStr<other_size, C> &other) const {
+    debug_assert(data_[size_in] == '\0');
+    debug_assert(other.data_[other_size] == '\0');
+
+    StaticSizedStr<size_in + other_size, C> out;
+    std::copy(data_.begin(), data_.end() - 1, out.data_.begin());
+    std::copy(other.data_.begin(), other.data_.end(),
+              out.data_.begin() + size_in);
+    debug_assert(out.data_[out.size()] == '\0');
+
+    return out;
   }
 
-  constexpr const char *c_str() const { return data_.data(); }
+  template <unsigned to_remove>
+  requires(to_remove <= size_in) constexpr auto remove_prefix() const {
+    StaticSizedStr<size_in - to_remove, C> out;
+    std::copy(data_.begin() + to_remove, data_.end(), out.data_.begin());
+    debug_assert(out.data_[out.size()] == '\0');
+
+    return out;
+  }
+
+  template <unsigned to_remove>
+  requires(to_remove <=
+           size_in) constexpr auto remove_prefix(IdxT<to_remove>) const {
+    return remove_prefix<to_remove>();
+  }
+
+  template <unsigned to_remove>
+  requires(to_remove <= size_in) constexpr auto remove_suffix() const {
+    StaticSizedStr<size_in - to_remove, C> out;
+    std::copy(data_.begin(), data_.end() - (to_remove + 1), out.data_.begin());
+    out.data_[out.size()] = '\0';
+
+    return out;
+  }
+
+  template <unsigned to_remove>
+  requires(to_remove <=
+           size_in) constexpr auto remove_suffix(IdxT<to_remove>) const {
+    return remove_suffix<to_remove>();
+  }
+
+  template <unsigned n> constexpr auto rep() const {
+    StaticSizedStr<n * size_in, C> out;
+    for (unsigned i = 0; i < n; ++i) {
+      std::copy(data_.begin(), data_.end() - 1,
+                out.data_.begin() + i * size_in);
+    }
+    out.data_[out.size()] = '\0';
+
+    return out;
+  }
+
+  template <unsigned n> constexpr auto rep(IdxT<n>) const { return rep<n>(); }
+
+  template <unsigned first_size, unsigned... rest_size>
+  constexpr auto join(const StaticSizedStr<first_size, C> &first,
+                      const StaticSizedStr<rest_size, C> &...rest) const {
+    return (first + ... + (*this + rest));
+  }
+
+  template <unsigned n, unsigned joined_size>
+  constexpr auto join_n(const StaticSizedStr<joined_size, C> &joined) const {
+    if constexpr (n == 0) {
+      return StaticSizedStr<0, C>{};
+    } else {
+      return (joined + *this).template rep<n - 1>() + joined;
+    }
+  }
+
+  template <unsigned n, unsigned joined_size>
+  constexpr auto join_n(IdxT<n>,
+                        const StaticSizedStr<joined_size, C> &joined) const {
+    return join_n<n>(joined);
+  }
 
   constexpr auto operator<=>(const StaticSizedStr &other) const = default;
 
 private:
-  template <unsigned other_size> friend class StaticSizedStr;
-
-  constexpr StaticSizedStr() {}
+  template <unsigned other_size, typename OtherC> friend class StaticSizedStr;
 
   // private to ensure null termination
-  std::array<char, size_in + 1> data_;
+  std::array<C, size_in + 1> data_;
 };
 
-template <const char *v>
-StaticSizedStr(std::integral_constant<const char *, v>)
-    -> StaticSizedStr<constexpr_strlen(v)>;
+template <typename T> struct IsStaticSizedStrImpl : std::false_type {};
 
 template <unsigned size>
-StaticSizedStr(char const (&)[size]) -> StaticSizedStr<size>;
+struct IsStaticSizedStrImpl<StaticSizedStr<size>> : std::true_type {};
 
-// namespace to avoid potential conflicts with short name
-namespace static_sized_str {
-template <unsigned arr_size>
-requires(arr_size != 0) constexpr StaticSizedStr<arr_size - 1> s_str(
-    char const (&v)[arr_size]) {
+template <typename T>
+concept IsStaticSizedStr = IsStaticSizedStrImpl<std::decay_t<T>>::value;
+
+template <typename C, const C *v>
+StaticSizedStr(std::integral_constant<const C *, v>)
+    -> StaticSizedStr<constexpr_strlen(v), C>;
+
+template <unsigned arr_size, typename C>
+StaticSizedStr(C const (&)[arr_size]) -> StaticSizedStr<arr_size - 1, C>;
+
+// in namespace because of short name...
+namespace short_func {
+// consider this as an equivalent to a user defined string literal
+template <unsigned arr_size, typename C>
+requires(arr_size !=
+         0) constexpr StaticSizedStr<arr_size - 1> s(C const (&v)[arr_size]) {
   // must be null terminated...
   debug_assert(v[arr_size - 1] == '\0');
 
   return {v};
 }
+} // namespace short_func
+
 } // namespace static_sized_str
--- tests/src/static_sized_str.cpp
+++ tests/src/static_sized_str.cpp
@@ -3,6 +3,7 @@
 #include <string_view>
 
 using namespace static_sized_str;
+using namespace short_func;
 using namespace std::literals;
 
 template <unsigned size>
@@ -10,24 +11,66 @@ constexpr std::string_view to_view(const StaticSizedStr<size> &v) {
   return std::string_view(v.c_str(), v.size());
 }
 
+template <unsigned n> constexpr auto idx_v = IdxT<n>{};
+
 static_assert([] {
-  constexpr auto empty = s_str("");
+  constexpr auto empty = s("");
 
   static_assert(empty.size() == 0);
   static_assert(constexpr_strlen(empty.c_str()) == 0);
   static_assert(to_view(empty) == ""sv);
+  static_assert(empty.remove_prefix<0>() == empty);
+  static_assert(empty.remove_suffix<0>() == empty);
+  static_assert(empty.remove_prefix(idx_v<0>) == empty);
+  static_assert(empty.remove_suffix(idx_v<0>) == empty);
+  static_assert(empty.rep<0>() == empty);
+  static_assert(empty.rep<1>() == empty);
+  static_assert(empty.rep(idx_v<0>) == empty);
+  static_assert(empty.rep<7>() == empty);
+  static_assert(empty.join(s("a")) == s("a"));
+  static_assert(empty.join(s("a"), s("b"), s("c")) == s("abc"));
+  static_assert(empty.join_n<3>(s("a")) == s("aaa"));
+  static_assert(empty.join_n(idx_v<3>, s("a")) == s("aaa"));
 
-  constexpr auto single = s_str("a");
+  constexpr auto single = s("a");
 
   static_assert(single.size() == 1);
   static_assert(constexpr_strlen(single.c_str()) == 1);
   static_assert(to_view(single) == "a"sv);
+  static_assert(single.remove_prefix<0>() == single);
+  static_assert(single.remove_suffix<0>() == single);
+  static_assert(single.remove_prefix<1>() == empty);
+  static_assert(single.remove_suffix<1>() == empty);
+  static_assert(single.remove_prefix(idx_v<1>) == empty);
+  static_assert(single.remove_suffix(idx_v<1>) == empty);
+  static_assert(single.rep<0>() == empty);
+  static_assert(single.rep<1>() == single);
+  static_assert(single.rep<4>() == s("aaaa"));
+  static_assert(single.join(s("b"), s("c")) == s("bac"));
+  static_assert(single.join(s("b")) == s("b"));
+  static_assert(single.join_n<3>(s("b")) == s("babab"));
 
-  constexpr auto many = s_str("a   a bc");
+  constexpr auto many = s("a   a bc");
 
   static_assert(many.size() == 8);
   static_assert(constexpr_strlen(many.c_str()) == 8);
   static_assert(to_view(many) == "a   a bc"sv);
+  static_assert(many.remove_prefix<0>() == many);
+  static_assert(many.remove_suffix<0>() == many);
+  static_assert(many.remove_suffix<2>() == s("a   a "));
+  static_assert(many.remove_prefix<1>() == s("   a bc"));
+  static_assert(many.remove_prefix<3>() == s(" a bc"));
+  static_assert(many.remove_prefix<8>() == empty);
+  static_assert(many.remove_suffix<8>() == empty);
+  static_assert(many.rep<0>() == empty);
+  static_assert(many.rep<1>() == many);
+  static_assert(many.rep<4>() == s("a   a bc"
+                                   "a   a bc"
+                                   "a   a bc"
+                                   "a   a bc"));
+
+  constexpr auto other_many = StaticSizedStr("a   a bc");
+  static_assert(other_many == many);
 
   return true;
 }());
