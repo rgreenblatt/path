93088adb27ff98b3df28ad715676fd8debf40d25
rgreenblatt <greenblattryan@gmail.com>
2021-01-20 20:13:41 -0500

    reduce benchmark

---
+++ benchmarks/src/reduce.cu
@@ -0,0 +1,433 @@
+#ifndef CPU_ONLY
+#include "data_structure/copyable_to_vec.h"
+#include "execution_model/device_vector.h"
+#include "execution_model/host_vector.h"
+#include "lib/bit_utils.h"
+#include "lib/cuda/reduce.cuh"
+#include "lib/cuda/utils.h"
+#include "lib/span.h"
+#include "lib/tagged_tuple.h"
+#include "lib/tagged_union.h"
+#include "meta/all_values.h"
+#include "meta/all_values_as_tuple.h"
+#include "meta/all_values_enum.h"
+#include "meta/all_values_integral.h"
+#include "meta/all_values_pow_2.h"
+#include "meta/as_tuple_macro.h"
+#include "meta/dispatch.h"
+#include "meta/tuple.h"
+
+#include <benchmark/benchmark.h>
+#include <cub/block/block_reduce.cuh>
+#include <cub/util_ptx.cuh>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/random.h>
+#include <thrust/transform.h>
+
+#include <sstream>
+
+enum class FullyDispatchedReduceImpl {
+  MineGeneric,
+  CUBGeneric,
+  CUBSum,
+};
+
+constexpr auto sum = [](const auto &l, const auto &r) { return l + r; };
+
+template <FullyDispatchedReduceImpl type, unsigned block_size,
+          unsigned items_per_thread, typename T>
+__global__ void block_reduce_fully_dispatched(Span<const T> in, Span<T> out) {
+  // block load doesn't seem to ever work...
+  T thread_data[items_per_thread];
+#pragma unroll
+  for (unsigned i = 0; i < items_per_thread; ++i) {
+    thread_data[i] =
+        in[i + items_per_thread * (threadIdx.x + blockIdx.x * blockDim.x)];
+  }
+
+  using BlockReduce = cub::BlockReduce<T, block_size>;
+  const T item = [&] {
+    if constexpr (type == FullyDispatchedReduceImpl::MineGeneric) {
+      T value = 0;
+#pragma unroll
+      for (unsigned i = 0; i < items_per_thread; ++i) {
+        value += thread_data[i];
+      }
+      return block_reduce(value, sum, threadIdx.x, block_size);
+    } else if constexpr (type == FullyDispatchedReduceImpl::CUBGeneric) {
+      __shared__ typename BlockReduce::TempStorage reduce_storage;
+      return BlockReduce(reduce_storage).Reduce(thread_data, sum);
+    } else {
+      static_assert(type == FullyDispatchedReduceImpl::CUBSum);
+      __shared__ typename BlockReduce::TempStorage reduce_storage;
+      return BlockReduce(reduce_storage).Sum(thread_data);
+    }
+  }();
+  if (threadIdx.x == 0) {
+    out[blockIdx.x] = item;
+  }
+}
+
+template <FullyDispatchedReduceImpl type, unsigned block_size,
+          unsigned sub_warp_size, unsigned items_per_thread, typename T>
+__global__ void warp_reduce_fully_dispatched(Span<const T> in, Span<T> out) {
+  using WarpReduce = cub::WarpReduce<T, sub_warp_size>;
+  T value = 0;
+#pragma unroll
+  for (unsigned i = 0; i < items_per_thread; ++i) {
+    value += in[i + items_per_thread * (threadIdx.x + blockIdx.x * blockDim.x)];
+  }
+
+  const T item = [&] {
+    if constexpr (type == FullyDispatchedReduceImpl::MineGeneric) {
+      return warp_reduce(value, sum, sub_warp_size);
+    } else if constexpr (type == FullyDispatchedReduceImpl::CUBGeneric) {
+      __shared__ typename WarpReduce::TempStorage reduce_storage;
+      return WarpReduce(reduce_storage).Reduce(value, sum);
+    } else {
+      static_assert(type == FullyDispatchedReduceImpl::CUBSum);
+      __shared__ typename WarpReduce::TempStorage reduce_storage;
+      return WarpReduce(reduce_storage).Sum(value);
+    }
+  }();
+  if (threadIdx.x % sub_warp_size == 0) {
+    constexpr unsigned sub_warps_per_block = block_size / sub_warp_size;
+    out[threadIdx.x / sub_warp_size + blockIdx.x * sub_warps_per_block] = item;
+  }
+}
+
+enum class NotFullyDispatchedReduceImpl {
+  Mine,
+};
+
+template <NotFullyDispatchedReduceImpl type, typename T>
+__global__ void block_reduce_not_fully_dispatched(Span<const T> in, Span<T> out,
+                                                  unsigned block_size,
+                                                  unsigned items_per_thread) {
+  // no others for now...
+  static_assert(type == NotFullyDispatchedReduceImpl::Mine);
+  debug_assert(block_size == blockDim.x);
+  debug_assert(threadIdx.x < block_size);
+  debug_assert_assume(block_size % warp_size == 0);
+  debug_assert_assume(power_of_2(block_size));
+  debug_assert_assume(power_of_2(items_per_thread)); // somewhat atypical...
+  T value = 0;
+  for (unsigned i = 0; i < items_per_thread; ++i) {
+    value += in[i + items_per_thread * (threadIdx.x + blockIdx.x * block_size)];
+  }
+  value = block_reduce(value, sum, threadIdx.x, block_size);
+  if (threadIdx.x == 0) {
+    out[blockIdx.x] = value;
+  }
+}
+
+template <NotFullyDispatchedReduceImpl type, typename T>
+__global__ void warp_reduce_not_fully_dispatched(Span<const T> in, Span<T> out,
+                                                 unsigned block_size,
+                                                 unsigned sub_warp_size,
+                                                 unsigned items_per_thread) {
+  static_assert(type == NotFullyDispatchedReduceImpl::Mine);
+  debug_assert(block_size == blockDim.x);
+  debug_assert(threadIdx.x < block_size);
+  debug_assert_assume(block_size % warp_size == 0);
+  debug_assert_assume(warp_size % sub_warp_size == 0);
+  debug_assert_assume(sub_warp_size <= warp_size);
+  debug_assert_assume(power_of_2(block_size));
+  debug_assert_assume(power_of_2(sub_warp_size));
+  debug_assert_assume(power_of_2(items_per_thread)); // somewhat atypical...
+
+  T value = 0;
+#pragma unroll
+  for (unsigned i = 0; i < items_per_thread; ++i) {
+    value += in[i + items_per_thread * (threadIdx.x + blockIdx.x * block_size)];
+  }
+
+  value = warp_reduce(value, sum, sub_warp_size);
+  if (threadIdx.x % sub_warp_size == 0) {
+    unsigned sub_warps_per_block = block_size / sub_warp_size;
+    out[threadIdx.x / sub_warp_size + blockIdx.x * sub_warps_per_block] = value;
+  }
+}
+
+enum class ItemType {
+  Float,
+};
+
+template <ItemType type> auto get_item_type() {
+  if constexpr (type == ItemType::Float) {
+    return float();
+  }
+}
+
+template <ItemType type> auto generate_item_dist() {
+  if constexpr (type == ItemType::Float) {
+    return thrust::uniform_real_distribution(-100.f, 100.f);
+  }
+}
+
+template <ItemType type> using GetItemType = decltype(get_item_type<type>());
+
+struct BlockSize : public Pow2<32, 512> {
+  using Pow2<32, 512>::Pow2Gen;
+};
+
+struct ItemsPerThread : public Pow2<1, 64> {
+  using Pow2<1, 64>::Pow2Gen;
+};
+
+struct SubWarpSize : public Pow2<1, 32> {
+  using Pow2<1, 32>::Pow2Gen;
+};
+
+template <> struct AllValuesImpl<BlockSize> {
+  static constexpr auto values = std::array<BlockSize, 3>{32, 128, 512};
+};
+
+template <> struct AllValuesImpl<ItemsPerThread> {
+  static constexpr auto values = std::array<ItemsPerThread, 4>{1, 8, 32, 64};
+};
+
+template <> struct AllValuesImpl<SubWarpSize> {
+  static constexpr auto values = std::array<SubWarpSize, 3>{1, 8, 32};
+};
+
+static_assert(AllValuesEnumerable<BlockSize>);
+static_assert(AllValuesEnumerable<ItemsPerThread>);
+static_assert(AllValuesEnumerable<SubWarpSize>);
+
+template <bool is_block> struct Constants {
+  BlockSize block_size;
+  std::conditional_t<is_block, EmptyEnumerable, SubWarpSize> sub_warp_size;
+  ItemsPerThread items_per_thread;
+
+  AS_TUPLE_STRUCTURAL(Constants, block_size, sub_warp_size, items_per_thread);
+
+  constexpr auto operator<=>(const Constants &other) const = default;
+};
+
+template <bool is_fully_dispatched> struct KernelType {
+  std::conditional_t<is_fully_dispatched, FullyDispatchedReduceImpl,
+                     NotFullyDispatchedReduceImpl>
+      reduce_impl;
+  ItemType item_type;
+
+  AS_TUPLE_STRUCTURAL(KernelType, reduce_impl, item_type);
+
+  constexpr unsigned numeric_reduce_impl() const {
+    auto out = static_cast<unsigned>(reduce_impl);
+    if (!is_fully_dispatched) {
+      out += magic_enum::enum_count<FullyDispatchedReduceImpl>();
+    }
+    return out;
+  }
+  constexpr unsigned numeric_item_type() const {
+    return static_cast<unsigned>(item_type);
+  }
+
+  constexpr auto operator<=>(const KernelType &other) const = default;
+};
+
+struct RunType {
+  bool is_block;
+  bool is_fully_dispatched;
+
+  AS_TUPLE_STRUCTURAL(RunType, is_block, is_fully_dispatched);
+
+  constexpr auto operator<=>(const RunType &other) const = default;
+};
+
+template <RunType type> struct AllVars {
+  Constants<type.is_block> constants;
+  KernelType<type.is_fully_dispatched> kernel_type;
+
+  AS_TUPLE_STRUCTURAL(AllVars, constants, kernel_type);
+
+  constexpr auto operator<=>(const AllVars &other) const = default;
+};
+
+template <RunType type>
+constexpr auto get_comp_time_params(AllVars<type> params) {
+  if constexpr (type.is_fully_dispatched) {
+    return params;
+  } else {
+    return params.kernel_type;
+  }
+}
+
+using BenchmarkParams = TaggedUnionPerInstance<RunType, AllVars>;
+
+template <ItemType type>
+using HostVectorForType = HostVector<GetItemType<type>>;
+template <ItemType type>
+using DeviceVectorForType = DeviceVector<GetItemType<type>>;
+
+int main(int argc, char *argv[]) {
+  TaggedTuplePerInstance<ItemType, HostVectorForType> cpu_input_vec;
+  TaggedTuplePerInstance<ItemType, HostVectorForType> cpu_output_vec;
+  TaggedTuplePerInstance<ItemType, HostVectorForType> cpu_output_from_gpu_vec;
+  TaggedTuplePerInstance<ItemType, DeviceVectorForType> input_vec;
+  TaggedTuplePerInstance<ItemType, DeviceVectorForType> output_vec;
+
+  auto sizes = [] {
+    if constexpr (debug_build) {
+      return std::array{1u << 12, 1u << 14, 1u << 18};
+    } else {
+      return std::array{1u << 12, 1u << 18, 1u << 20,
+                        1u << 22, 1u << 24, 1u << 26};
+    }
+  }();
+  const unsigned max_size = sizes[sizes.size() - 1];
+  for (unsigned size : sizes) {
+    for (auto params : AllValues<BenchmarkParams>) {
+      params.visit_tagged([&](auto tag, auto params) {
+        constexpr RunType run_type = decltype(tag)::value;
+
+        dispatch(get_comp_time_params<run_type>(params), [&](auto tag) {
+          constexpr auto type = decltype(tag)::value;
+          const auto constants = params.constants;
+          const unsigned items_per_block =
+              constants.block_size * constants.items_per_thread;
+          if (size % items_per_block != 0) {
+            return;
+          }
+          const unsigned num_blocks = size / items_per_block;
+          unsigned reduction_factor = items_per_block;
+          if constexpr (!run_type.is_block) {
+            reduction_factor =
+                constants.sub_warp_size * constants.items_per_thread;
+          }
+
+          // // TODO
+          // if (reduction_factor == 1) {
+          //   return;
+          // }
+
+          const unsigned iters = [&] {
+            if constexpr (debug_build) {
+              return 1;
+            } else {
+              const unsigned base_iters = 8;
+              const unsigned max_iters = 128u;
+              return std::min(base_iters * max_size / size, max_iters);
+            }
+          }();
+
+          constexpr KernelType kernel_type = [&]() {
+            if constexpr (run_type.is_fully_dispatched) {
+              return type.kernel_type;
+            } else {
+              return type;
+            }
+          }();
+          std::stringstream name;
+          name << "reduce_" << size << "_"
+               << magic_enum::enum_name(kernel_type.reduce_impl) << "_"
+               << magic_enum::enum_name(kernel_type.item_type)
+               << "_reduction_factor_" << reduction_factor << "_block_size_"
+               << constants.block_size;
+          if constexpr (!run_type.is_block) {
+            name << "_sub_warp_size_" << constants.sub_warp_size;
+          }
+          name << "_items_per_thread_" << constants.items_per_thread;
+          benchmark::RegisterBenchmark(
+              name.str().c_str(),
+              [=, &cpu_input = cpu_input_vec.get(TagV<kernel_type.item_type>),
+               &cpu_output = cpu_output_vec.get(TagV<kernel_type.item_type>),
+               &cpu_output_from_gpu =
+                   cpu_output_from_gpu_vec.get(TagV<kernel_type.item_type>),
+               &input = input_vec.get(TagV<kernel_type.item_type>),
+               &output = output_vec.get(TagV<kernel_type.item_type>)](
+                  benchmark::State &st) {
+                st.counters["size"] = size;
+                st.counters["impl"] = kernel_type.numeric_reduce_impl();
+                st.counters["item_type"] = kernel_type.numeric_item_type();
+                st.counters["reduction_factor"] = reduction_factor;
+                st.counters["block_size"] = constants.block_size();
+                if constexpr (run_type.is_block) {
+                  st.counters["sub_warp_size"] = 0;
+                } else {
+                  st.counters["sub_warp_size"] = constants.sub_warp_size();
+                }
+                st.counters["items_per_thread"] = constants.items_per_thread();
+
+                input.resize(size);
+                auto dist = generate_item_dist<kernel_type.item_type>();
+                auto counter = thrust::make_counting_iterator(0u);
+                thrust::transform(counter, counter + size, input.begin(),
+                                  [=](unsigned n) mutable {
+                                    thrust::default_random_engine rng;
+                                    rng.discard(n);
+                                    return dist(rng);
+                                  });
+                output.resize(size / reduction_factor);
+                for (auto _ : st) {
+                  using ItemT = GetItemType<kernel_type.item_type>;
+
+                  if constexpr (run_type.is_fully_dispatched) {
+                    constexpr Constants constants = type.constants;
+                    if constexpr (run_type.is_block) {
+                      block_reduce_fully_dispatched<
+                          kernel_type.reduce_impl, constants.block_size,
+                          constants.items_per_thread, ItemT>
+                          <<<num_blocks, constants.block_size()>>>(input,
+                                                                   output);
+                    } else {
+                      warp_reduce_fully_dispatched<
+                          kernel_type.reduce_impl, constants.block_size,
+                          constants.sub_warp_size, constants.items_per_thread,
+                          ItemT><<<num_blocks, constants.block_size()>>>(
+                          input, output);
+                    }
+                  } else {
+                    if constexpr (run_type.is_block) {
+                      block_reduce_not_fully_dispatched<kernel_type.reduce_impl,
+                                                        ItemT>
+                          <<<num_blocks, constants.block_size()>>>(
+                              input, output, constants.block_size,
+                              constants.items_per_thread);
+                    } else {
+                      warp_reduce_not_fully_dispatched<kernel_type.reduce_impl,
+                                                       ItemT>
+                          <<<num_blocks, constants.block_size()>>>(
+                              input, output, constants.block_size,
+                              constants.sub_warp_size,
+                              constants.items_per_thread);
+                    }
+                  }
+                  CUDA_SYNC_CHK();
+                }
+                // TODO: generalize as needed...
+                if constexpr (debug_build &&
+                              kernel_type.item_type == ItemType::Float) {
+                  copy_to_vec(input, cpu_input);
+                  cpu_output.resize(size / reduction_factor);
+                  for (unsigned i = 0; i < size; i += reduction_factor) {
+                    float &v = cpu_output[i / reduction_factor];
+                    v = 0.f;
+                    for (unsigned j = i; j < i + reduction_factor; ++j) {
+                      v += cpu_input[j];
+                    }
+                  }
+
+                  copy_to_vec(output, cpu_output_from_gpu);
+
+                  for (unsigned i = 0; i < size / reduction_factor; ++i) {
+                    // correct epsilon will depend on distribution
+                    // multiplied by reduction factor because non-commutativity
+                    // differences accumulate
+                    debug_assert((cpu_output[i] - cpu_output_from_gpu[i]) <
+                                 reduction_factor * 1e-5);
+                  }
+                }
+              })
+              ->Unit(benchmark::kMillisecond)
+              ->Iterations(iters);
+        });
+      });
+    }
+  }
+
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+}
+#endif
--- scripts/plot_reduce_benchmark_runs.py
+++ scripts/plot_reduce_benchmark_runs.py
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 from benchmark_utils import get_benchmark_dicts, setup_directory
 
 Instance = namedtuple('Instance', 'item_type reduction_factor')
-SubInstance = namedtuple(
-    'SubInstance', 'file_name impl block_size sub_warp_size items_per_thread')
+Constants = namedtuple('Constants',
+                       'block_size sub_warp_size items_per_thread')
+Implementation = namedtuple('Implementation', 'file_name impl')
 
 
 # these mappings need to kept up to date with cpp....
@@ -27,10 +28,13 @@ def get_instance(run):
                     int(run['reduction_factor']))
 
 
-def get_sub_instance(file_name, run):
-    return SubInstance(file_name, str_reduce_impl(run['impl']),
-                       int(run['block_size']), int(run['sub_warp_size']),
-                       int(run['items_per_thread']))
+def get_constants(run):
+    return Constants(int(run['block_size']), int(run['sub_warp_size']),
+                     int(run['items_per_thread']))
+
+
+def get_implementation(file_name, run):
+    return Implementation(file_name, str_reduce_impl(run['impl']))
 
 
 def format_legend(file_names, sub_instance):
@@ -44,8 +48,8 @@ def main(json_files):
     json_files, benchmark_dicts = get_benchmark_dicts(json_files)
     directory = setup_directory('reduce')
 
-    instances = defaultdict(
-        lambda: defaultdict(lambda: defaultdict(lambda: [None] * 2)))
+    instances_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(
+        lambda: defaultdict(lambda: [None] * 2))))
     name_to_info = {}
     for file_name, runs in zip(json_files, benchmark_dicts):
         for run in runs:
@@ -53,45 +57,78 @@ def main(json_files):
             run_name = run['run_name']
             if run_type == 'iteration':
                 instance = get_instance(run)
-                sub_instance = get_sub_instance(file_name, run)
+                constants = get_constants(run)
+                impl = get_implementation(file_name, run)
                 size = int(run['size'])
-                name_to_info[run['run_name']] = (instance, sub_instance, size)
+                name_to_info[run['run_name']] = (instance, constants, impl,
+                                                 size)
                 continue
             assert run_type == 'aggregate'
-            instance, sub_instance, size = name_to_info[run_name]
+            instance, constants, impl, size = name_to_info[run_name]
 
             # if (sub_instance.items_per_thread != 1
             #         or sub_instance.block_size != 512):
             #     continue
-            item = instances[instance][sub_instance][size]
+            item = instances_dict[instance][constants][impl][size]
             if run['aggregate_name'] == 'mean':
                 item[0] = run['real_time']
             elif run['aggregate_name'] == 'stddev':
                 item[1] = 2 * run['real_time']
-    for instance, sub_instances in instances.items():
-        for size_dict in sub_instances.values():
-            sizes, times, errs = zip(*((size, time, err)
-                                       for size, (time,
-                                                  err) in size_dict.items()))
-            assert not any(size is None for size in sizes)
-            assert not any(time is None for time in times)
-            assert not any(err is None for err in errs)
-            plt.errorbar(sizes, times, yerr=errs, capsize=10.0)
 
-        plt.gcf().set_size_inches(15, 10)
-        legend = [
-            format_legend(json_files, sub_instance)
-            for sub_instance in sub_instances.keys()
-        ]
-        plt.xlabel('size')
-        plt.ylabel('time (ms)')
-        # fig.yscale('log')
-        plt.legend(legend, loc='lower right', ncol=1)
-        plt.savefig(
-            os.path.join(
-                directory, '{}_{}.png'.format(instance.item_type,
-                                              instance.reduction_factor)))
-        plt.clf()
+    compare_all_like = False
+    for instance, constants_dict in instances_dict.items():
+
+        def setup_save_fig(legend, file_name_no_ext):
+            size = (15, 10) if compare_all_like else (10, 8)
+            plt.gcf().set_size_inches(*size)
+            plt.xlabel('size')
+            plt.ylabel('time (ms)')
+            # fig.yscale('log')
+            plt.legend(legend, loc='lower right', ncol=1)
+            plt.savefig(
+                os.path.join(directory, '{}.png'.format(file_name_no_ext)))
+            plt.clf()
+
+        legend_items = []
+        base_file_name = '{}_factor_{}'.format(instance.item_type,
+                                               instance.reduction_factor)
+        for constants, impls_dict in constants_dict.items():
+            if not compare_all_like:
+                legend_items.clear()
+            for impl, size_dict in impls_dict.items():
+                sizes, times, errs = zip(
+                    *((size, time, err)
+                      for size, (time, err) in size_dict.items()))
+                assert not any(size is None for size in sizes)
+                assert not any(time is None for time in times)
+                assert not any(err is None for err in errs)
+                plt.errorbar(sizes, times, yerr=errs, capsize=10.0)
+
+                def formatting(named_tuple):
+                    return dict(named_tuple._asdict())
+
+                def remove_name_if(impl_dict):
+                    if len(json_files) == 1:
+                        del impl_dict['file_name']
+                    return impl_dict
+
+                legend_items.append(
+                    (formatting(constants), remove_name_if(formatting(impl))))
+
+            if not compare_all_like:
+                legend = [str(impl) for _, impl in legend_items]
+
+                f_name = '{}_block_size_{}_{}items_per_thread_{}'.format(
+                    base_file_name, constants.block_size,
+                    'sub_warp_size_{}_'.format(constants.sub_warp_size)
+                    if constants.sub_warp_size != 0 else '',
+                    constants.items_per_thread)
+
+                setup_save_fig(legend, f_name)
+
+        if compare_all_like:
+            legend = [str(v) for v in legend_items]
+            setup_save_fig(legend, base_file_name)
 
 
 if __name__ == "__main__":
