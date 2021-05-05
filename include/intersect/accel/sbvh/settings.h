350576f20441f670e47e3a50e4ac3e7a6fcfaa94
rgreenblatt <greenblattryan@gmail.com>
2021-05-04 21:06:59 -0400

    comment

--- include/intersect/accel/sbvh/settings.h
+++ include/intersect/accel/sbvh/settings.h
@@ -9,7 +9,10 @@ namespace sbvh {
 struct Settings {
   // alpha from original paper
   float overlap_threshold = 1e-5;
+
+  // spatial splits seem bad at the moment...
   bool use_spatial_splits = false;
+
   detail::bvh::Settings bvh_settings;
 
   SETTING_BODY(Settings, overlap_threshold, use_spatial_splits, bvh_settings);
