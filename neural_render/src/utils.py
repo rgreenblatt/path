d777641004a45fd9ed045b4634dec66c50eeaf95
rgreenblatt <greenblattryan@gmail.com>
2021-05-14 10:42:18 -0400

    also log stderr

--- neural_render/src/utils.py
+++ neural_render/src/utils.py
@@ -6,6 +6,20 @@ def mkdirs(path):
     Path(path).mkdir(parents=True, exist_ok=True)
 
 
+class StderrHolder():
+    def __init__(self, file):
+        self.file = file
+        self.stderr = sys.stderr
+        sys.stderr = self
+
+    def write(self, data):
+        self.file.write(data)
+        self.stderr.write(data)
+
+    def flush(self):
+        self.file.flush()
+
+
 class PrintAndLog():
     def __init__(self, file_name):
         super().__init__()
@@ -14,7 +28,10 @@ class PrintAndLog():
         self.stdout = sys.stdout
         sys.stdout = self
 
+        self.stderr_holder = StderrHolder(self.file)
+
     def __del__(self):
+        sys.stderr = self.stderr_holder.stderr
         sys.stdout = self.stdout
         self.file.close()
 
