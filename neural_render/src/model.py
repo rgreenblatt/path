0988ab4c083bc8b85736a99f9e007fea2b341657
rgreenblatt <greenblattryan@gmail.com>
2021-05-16 12:54:21 -0400

    Substantially altered arch based on results

--- neural_render/src/model.py
+++ neural_render/src/model.py
@@ -1,45 +1,50 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 
-class DenseBlock(nn.Module):
+class LinearAndMultiply(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
 
         self._activation = nn.CELU()
-        self._hidden_size_multiplier = 4
-        self._hidden_size = output_size * self._hidden_size_multiplier
-        self._pad_size = output_size - input_size
-        assert self._pad_size >= 0
-
-        self._expand = nn.Linear(input_size, self._hidden_size)
-        self._contract = nn.Linear(self._hidden_size, output_size)
-
-        self._mul_divider = 4
-        self._mul_size = output_size // self._mul_divider
-        self._output_mul_size = self._mul_size * self._mul_divider
-        self._contract_for_mul = nn.Linear(output_size, self._mul_size)
-
+        self._linear = nn.Linear(input_size, output_size)
+        self._to_multiplier = nn.Linear(output_size, output_size)
         self._norm = nn.LayerNorm(output_size)
 
     def forward(self, x):
-        padding = torch.zeros(*x.size()[:-1],
-                              self._pad_size,
-                              device=x.device,
-                              dtype=x.dtype)
-        padded_input = torch.cat((x, padding), -1)
-        x = self._activation(self._contract(self._activation(self._expand(x))))
+        x = self._activation(self._linear(x))
+        return self._norm(x * torch.tanh(self._to_multiplier(x)))
 
-        x_for_mul = x[..., :self._output_mul_size]
-        x_not_mul = x[..., self._output_mul_size:]
 
-        sub_shape = x_for_mul.size()[:-1]
-        multiplier = torch.tanh(self._contract_for_mul(x)).unsqueeze(-1)
-        x_mul = multiplier * x_for_mul.view(*sub_shape, self._mul_size, -1)
+def interpolate_sizes(input_size, output_size, count):
+    per = (output_size - input_size) / count
+    last_size = input_size
+    for i in range(count):
+        new_size = round(per * (i + 1) + input_size)
+        yield last_size, new_size
+        last_size = new_size
 
-        x = torch.cat((x_mul.view(*sub_shape, -1), x_not_mul), -1)
+    assert last_size == output_size
 
-        return self._norm(padded_input + x)
+
+class ResBlock(nn.Module):
+    def __init__(self, input_size, output_size):
+        super().__init__()
+
+        n_subblocks = 2
+        sizes = interpolate_sizes(input_size, output_size, n_subblocks)
+        self._subblocks = nn.ModuleList(
+            [LinearAndMultiply(inp, out) for inp, out in sizes])
+
+        self._pad_size = output_size - input_size
+        assert self._pad_size >= 0
+
+    def forward(self, x):
+        padded_input = F.pad(x, (0, self._pad_size))
+        for block in self._subblocks:
+            x = block
+        return padded_input + x
 
 
 class Net(nn.Module):
@@ -57,22 +62,17 @@ class Net(nn.Module):
 
         self._n_blocks = 8
 
-        self._scene_blocks = nn.ModuleList()
-
-        per_block_size = (self._end_size - self._start_size) / self._n_blocks
-        last_size = self._start_size
-        for i in range(self._n_blocks):
-            new_block_size = round(per_block_size * (i + 1) + self._start_size)
-            self._scene_blocks.append(DenseBlock(last_size, new_block_size))
-            last_size = new_block_size
-        assert last_size == self._end_size
+        scene_sizes = interpolate_sizes(self._start_size, self._end_size,
+                                        self._n_blocks)
+        self._scene_blocks = nn.ModuleList(
+            [ResBlock(inp, out) for inp, out in scene_sizes])
 
         self._coord_size = 2
         self._coord_block_size = 32
         self._coords_expand = nn.Linear(self._coord_size,
                                         self._coord_block_size)
-        self._coords_block = DenseBlock(self._coord_block_size,
-                                        self._coord_block_size)
+        self._coords_block = ResBlock(self._coord_block_size,
+                                      self._coord_block_size)
         self._coords_to_multiplier = nn.Linear(self._coord_block_size,
                                                self._multiplier_size)
 
@@ -83,8 +83,8 @@ class Net(nn.Module):
 
         self._to_output_block = nn.Linear(self._multiplier_size,
                                           self._output_block_size)
-        self._output_block = DenseBlock(self._output_block_size,
-                                        self._output_block_size)
+        self._output_block = ResBlock(self._output_block_size,
+                                      self._output_block_size)
 
         self._output_size = 3
 
@@ -102,3 +102,10 @@ class Net(nn.Module):
         out = self._to_output_block(torch.unsqueeze(x, 1) * multiplier) + addr
 
         return torch.relu(self._output(self._output_block(out)))
+
+
+if __name__ == "__main__":
+    assert len(list(interpolate_sizes(128, 507, 8))) == 8
+    assert len(list(interpolate_sizes(507, 507, 8))) == 8
+    for inp_s, out_s in interpolate_sizes(128, 507, 8):
+        print("inp:", inp_s, "out:", out_s)
