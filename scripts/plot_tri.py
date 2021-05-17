b7cef65ed42a973ad5e3481809731186ccda6765
rgreenblatt <greenblattryan@gmail.com>
2021-05-17 17:14:54 -0400

    Much better triangle + misc plotter

--- scripts/plot_tri.py
+++ scripts/plot_tri.py
@@ -1,49 +1,23 @@
-from itertools import product, combinations
-
-import mpl_toolkits.mplot3d as a3
-import matplotlib.pyplot as plt
+from mayavi import mlab
 import numpy as np
 
 
-def draw_tri(ax, vertices, edge_color='k'):
-    tri = a3.art3d.Poly3DCollection([vertices])
-    tri.set_edgecolor(edge_color)
-    ax.add_collection3d(tri)
+def draw_tri(vertices, color=(0, 0, 0)):
+    mlab.triangular_mesh(vertices[:, 0],
+                         vertices[:, 1],
+                         vertices[:, 2], [[0, 1, 2]],
+                         color=color)
 
 
-def connect_tris(ax, vertices_first, vertices_second, edge_color='k'):
+def connect_tris(vertices_first, vertices_second, color=(0, 0, 0)):
     for i in range(3):
         for j in range(3):
-            ax.plot3D(*zip(vertices_first[i], vertices_second[j]),
-                      color=edge_color)
-
-
-def draw_aabb(ax, min_bound, max_bound, color='b'):
-    grid = 2
-    for (axis, edge_next, edge_next_next) in product([0, 1, 2],
-                                                     range(grid + 1),
-                                                     range(grid + 1)):
-        s = [0] * len(min_bound)
-        e = [0] * len(min_bound)
-
-        s[axis] = min_bound[axis]
-        e[axis] = max_bound[axis]
-
-        next_axis = (axis + 1) % 3
-        next_next = (axis + 2) % 3
-
-        s[next_axis] = e[next_axis] = (
-            max_bound[next_axis] -
-            min_bound[next_axis]) * edge_next / grid + min_bound[next_axis]
-        s[next_next] = e[next_next] = (
-            max_bound[next_next] - min_bound[next_next]
-        ) * edge_next_next / grid + min_bound[next_next]
-        ax.plot3D(*zip(s, e), color=color)
+            mlab.plot3d(*[[vertices_first[i][k], vertices_second[j][k]]
+                          for k in range(3)],
+                        color=color)
 
 
 def main():
-    ax = a3.Axes3D(plt.figure())
-
     triangle_0 = [
         [0.441879, -0.528226, 0.629064],
         [0.997233, -0.289026, -0.629806],
@@ -62,26 +36,17 @@ def main():
         [0.330397, -0.306568, -0.741083],
     ]
 
-    draw_tri(ax, triangle_0, edge_color='r')
-    draw_tri(ax, triangle_1, edge_color='g')
-    draw_tri(ax, triangle_2, edge_color='b')
-    # connect_tris(ax, triangle_0, triangle_2, edge_color='g')
+    (triangle_0, triangle_1, triangle_2) = (np.array(triangle_0),
+                                            np.array(triangle_1),
+                                            np.array(triangle_2))
 
-    ax.scatter([-0.641504], [ -2.92644], [-0.976747])
+    mlab.figure(fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
 
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
-    ax.set_xlim(-2., 2.)
-    ax.set_ylim(-2., 2.)
-    ax.set_zlim(-2., 2.)
-
-    # end = np.array([0.52142638, 0.36578992, 0.182400629])
-    # origin = np.array([0.362145573, 0.0209254418, 0.572940052])
-    # direction = end - origin
-    # ax.quiver(*origin, *direction * 2, color='r')
-
-    plt.show()
+    draw_tri(triangle_0, color=(0, 0, 1))
+    draw_tri(triangle_1, color=(0, 1, 0))
+    draw_tri(triangle_2, color=(1, 0, 0))
+    connect_tris(triangle_0, triangle_2, color=(1, 1, 0))
+    mlab.show()
 
 
 if __name__ == "__main__":
