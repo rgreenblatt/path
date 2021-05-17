52524380159ecbe563b784e47f0dd47238c6327b
rgreenblatt <greenblattryan@gmail.com>
2021-05-17 16:56:08 -0400

    Add unary minus to unitvector and minor improvement

--- include/lib/unit_vector.h
+++ include/lib/unit_vector.h
@@ -12,14 +12,14 @@ template <typename T> class UnitVectorGen {
 public:
   HOST_DEVICE UnitVectorGen() : v_({1., 0., 0.}) {}
 
+  // NOTE: can't be zero vector (or very close to zero)
   ATTR_PURE_NDEBUG HOST_DEVICE static UnitVectorGen
   new_normalize(const Eigen::Vector3<T> &v) {
-    return UnitVectorGen(v.normalized());
+    return new_unchecked(v.normalized());
   }
 
   ATTR_PURE_NDEBUG HOST_DEVICE static UnitVectorGen
   new_unchecked(const Eigen::Vector3<T> &v) {
-    // TODO: epsilon?
     debug_assert(std::abs(v.norm() - 1.) < 1e-6);
     return UnitVectorGen(v);
   }
@@ -29,6 +29,13 @@ public:
   }
 
   HOST_DEVICE const Eigen::Vector3<T> *operator->() const { return &v_; }
+
+  // unary minus is allowed
+  ATTR_PURE_NDEBUG HOST_DEVICE UnitVectorGen operator-() const {
+    UnitVectorGen out;
+    out.v_ = -v_;
+    return out;
+  }
 
 private:
   HOST_DEVICE explicit UnitVectorGen(const Eigen::Vector3<T> &v) : v_(v) {}
