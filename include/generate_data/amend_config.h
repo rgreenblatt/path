60efd58e5f9e3dc1ee85176b94cc10b05f7a4991
rgreenblatt <greenblattryan@gmail.com>
2021-05-17 17:14:34 -0400

    inline -> static

--- include/generate_data/amend_config.h
+++ include/generate_data/amend_config.h
@@ -3,7 +3,7 @@
 #include "render/settings.h"
 
 namespace generate_data {
-inline void amend_config(render::Settings &settings) {
+static void amend_config(render::Settings &settings) {
   settings.term_prob = {
       tag_v<integrate::term_prob::enum_term_prob::TermProbType::NIters>,
       integrate::term_prob::n_iters::Settings{.iters = 0}};
